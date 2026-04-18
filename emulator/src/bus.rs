06269942cfffe0f569f93edb9fb58f92ea0affc1
Young Seo Kim <youngseokim7@gmail.com>
2026-04-18 17:58:53 +0900

    implement basic bus structure

--- emulator/src/bus.rs
+++ emulator/src/bus.rs
@@ -1,1 +1,20 @@
+use crate::bus::cartridge::Cartridge;
+
 pub mod cartridge;
+
+pub struct Bus {
+    cartridge: Cartridge,
+}
+
+impl Bus {
+    pub fn new(cartridge: Cartridge) -> Self {
+        Self { cartridge }
+    }
+
+    pub fn read_u8(&self, address: u16) -> u8 {
+        match address {
+            0x0000..=0x7FFF => self.cartridge.read_u8(address),
+            _ => todo!("other bus things"),
+        }
+    }
+}
