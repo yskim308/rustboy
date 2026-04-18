3d3575dc6a040d6e7ab107153f59fb95ecae9e87
Young Seo Kim <youngseokim7@gmail.com>
2026-04-18 18:30:33 +0900

    CPU struct scaffolding

--- emulator/src/cpu.rs
+++ emulator/src/cpu.rs
@@ -1,1 +1,21 @@
+use crate::{bus::Bus, cpu::register::Registers};
+
 pub mod register;
+
+pub struct Cpu {
+    registers: Registers,
+    // todo: Other CPU state like interrupts
+}
+
+impl Cpu {
+    pub fn new() -> Self {
+        let registers = Registers::new();
+        Self { registers }
+    }
+
+    pub fn execute(&mut self, opcode: u8, bus: &mut Bus) {
+        match opcode {
+            _ => panic!("Unimplemented opcode: {opcode} at pc {}", self.registers.pc),
+        }
+    }
+}
