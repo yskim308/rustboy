6577a9ff1fbc93a1b4e90903d990ae3f51dfe025
Young Seo Kim <youngseokim7@gmail.com>
2026-04-18 18:05:49 +0900

    Load ROM into memory

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,6 +1,23 @@
+use std::{fs, io};
+
+use crate::bus::{
+    Bus,
+    cartridge::{self, Cartridge},
+};
+
 mod bus;
 mod cpu;
 
 fn main() {
-    println!("Hello, world!");
+    let mut file_path = String::new();
+
+    println!("Path (path/to/rom): ");
+    io::stdin()
+        .read_line(&mut file_path)
+        .expect_err("Failed while reading input: ");
+
+    let rom_data = fs::read(file_path).expect("Failed to read ROM from given file path");
+
+    let cartridge = Cartridge::new(rom_data);
+    let mut bus = Bus::new(cartridge);
 }
