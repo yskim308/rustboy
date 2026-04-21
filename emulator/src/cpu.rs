4e448726a530560373484413e634382b4920a344
Young Seo Kim <youngseokim7@gmail.com>
2026-04-21 16:48:22 +0900

    skeleton for fetching addresses, and intiial jump instruction (0xC3)

--- emulator/src/cpu.rs
+++ emulator/src/cpu.rs
@@ -23,9 +23,27 @@ impl Cpu {
         self.execute(read_byte, bus)
     }
 
+    fn fetch_u8(&mut self, bus: &mut Bus) -> u8 {
+        let value = bus.read_u8(self.registers.pc);
+        self.registers.pc = self.registers.pc.wrapping_add(1);
+        value
+    }
+
+    fn fetch_u16(&mut self, bus: &mut Bus) -> u16 {
+        let low = self.fetch_u8(bus);
+        let high = self.fetch_u8(bus);
+        let value = u16::from_le_bytes([low, high]);
+        value
+    }
+
     pub fn execute(&mut self, opcode: u8, bus: &mut Bus) -> u8 {
         match opcode {
             0x00 => 4,
+            0xC3 => {
+                let value = self.fetch_u16(bus);
+                self.registers.pc = value;
+                16
+            }
             _ => panic!("Unimplemented opcode: {opcode} at pc {}", self.registers.pc),
         }
     }
