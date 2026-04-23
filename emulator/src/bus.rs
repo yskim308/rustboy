8188ffaec81a2efa31346015879b5d091788bcd9
Young Seo Kim <youngseokim7@gmail.com>
2026-04-23 21:55:33 +0900

    implement writing to wram and IO

--- emulator/src/bus.rs
+++ emulator/src/bus.rs
@@ -34,10 +34,30 @@ impl Bus {
         }
     }
 
+    pub fn write_u8(&mut self, address: u16, data: u8) {
+        match address {
+            0x0000..=0x7FF => todo!("implement writing to ROM for MBC control"),
+            0xC000..=0xDFFF => self.wram.write_u8(address - 0xC000, data),
+            0xE000..=0xFDFF => self.wram.write_u8(address - 0xE000, data), // echo ram
+            0xFF00..=0xFF70 => self.write_io(address, data),
+            _ => {
+                eprintln!("Memory address {address} not implemented yet in Bus");
+                todo!("handle")
+            }
+        }
+    }
+
     fn read_io(&self, address: u16) -> u8 {
         match address {
             0xFF01..=0xFF02 => self.serial.read(address),
             _ => self.io_bucket[address as usize],
+        }
+    }
+
+    fn write_io(&mut self, address: u16, data: u8) {
+        match address {
+            0xFF01..=0xFF02 => self.serial.write(address, data),
+            _ => self.io_bucket[address as usize] = data,
         }
     }
 }
