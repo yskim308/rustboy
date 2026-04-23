ea38c1af4c397ddbe41aa68742c174399b2b9729
Young Seo Kim <youngseokim7@gmail.com>
2026-04-23 19:39:16 +0900

    temporary IO stub

--- emulator/src/bus.rs
+++ emulator/src/bus.rs
@@ -6,6 +6,7 @@ pub mod wram;
 pub struct Bus {
     cartridge: Cartridge,
     wram: Wram,
+    io_bucket: [u8; 128], // temporary for io stubs
 }
 
 impl Bus {
@@ -13,6 +14,7 @@ impl Bus {
         Self {
             cartridge,
             wram: Wram::new(),
+            io_bucket: [0; 128],
         }
     }
 
@@ -21,10 +23,17 @@ impl Bus {
             0x0000..=0x7FFF => self.cartridge.read_u8(address),
             0xC000..=0xDFFF => self.wram.read_u8(address - 0xC000),
             0xE000..=0xFDFF => self.wram.read_u8(address - 0xE000), // echo ram
+            0xFF00..=0xFF70 => self.read_io(address),
             _ => {
                 eprintln!("Memory address {address} not implemented yet in Bus");
                 todo!("handle")
             }
+        }
+    }
+
+    fn read_io(&self, address: u16) -> u8 {
+        match address {
+            _ => self.io_bucket[address as usize],
         }
     }
 }
