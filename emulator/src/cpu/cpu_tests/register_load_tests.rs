dcec68fcd6a6cf65c365a3743db27dc226b904b6
Young Seo Kim <youngseokim7@gmail.com>
2026-04-23 18:27:45 +0900

    Complete pure register opcodes with unit tests

--- emulator/src/cpu/cpu_tests/register_load_tests.rs
+++ emulator/src/cpu/cpu_tests/register_load_tests.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+    #![allow(non_snake_case)]
+
     use crate::{
         bus::{Bus, cartridge::Cartridge},
         cpu::Cpu,
@@ -10,84 +12,149 @@ mod tests {
         cpu.registers.pc = 0x0100;
 
         let mut rom = vec![0; 0x8000];
-
         for (i, &byte) in instruction_bytes.iter().enumerate() {
             rom[0x0100 + i] = byte;
         }
 
         let bus = Bus::new(Cartridge::new(rom));
-
         (cpu, bus)
     }
 
-    #[test]
-    fn execute_0x0E() {
-        // LD C u8, set C to u8, takes 8 cycles
-        let (mut cpu, mut bus) = setup(&[0x12]);
+    macro_rules! test_ld_r_u8 {
+        ($name:ident, $opcode:expr, $register:ident, $value:expr) => {
+            #[test]
+            fn $name() {
+                let (mut cpu, mut bus) = setup(&[$value]);
 
-        let cycles = cpu.execute(0x0E, &mut bus);
+                let cycles = cpu.execute($opcode, &mut bus);
+
+                assert_eq!(cycles, 8);
+                assert_eq!(cpu.registers.$register, $value);
+            }
+        };
+    }
+
+    macro_rules! test_ld_rr_u16 {
+        ($name:ident, $opcode:expr, $getter:ident) => {
+            #[test]
+            fn $name() {
+                let (mut cpu, mut bus) = setup(&[0x34, 0x12]);
+
+                let cycles = cpu.execute($opcode, &mut bus);
+
+                assert_eq!(cycles, 12);
+                assert_eq!(cpu.registers.$getter(), 0x1234);
+            }
+        };
+    }
+
+    macro_rules! test_ld_r_r {
+        ($name:ident, $opcode:expr, $dst:ident, $src:ident, $value:expr) => {
+            #[test]
+            fn $name() {
+                let (mut cpu, mut bus) = setup(&[]);
+                cpu.registers.$src = $value;
+
+                let cycles = cpu.execute($opcode, &mut bus);
+
+                assert_eq!(cycles, 4);
+                assert_eq!(cpu.registers.$dst, $value);
+                assert_eq!(cpu.registers.$src, $value);
+            }
+        };
+    }
+
+    test_ld_rr_u16!(execute_0x01, 0x01, get_bc);
+    test_ld_r_u8!(execute_0x06, 0x06, b, 0x12);
+    test_ld_r_u8!(execute_0x0E, 0x0E, c, 0x12);
+    test_ld_rr_u16!(execute_0x11, 0x11, get_de);
+    test_ld_r_u8!(execute_0x16, 0x16, d, 0x12);
+    test_ld_r_u8!(execute_0x1E, 0x1E, e, 0x12);
+    test_ld_rr_u16!(execute_0x21, 0x21, get_hl);
+    test_ld_r_u8!(execute_0x26, 0x26, h, 0x12);
+    test_ld_r_u8!(execute_0x2E, 0x2E, l, 0x12);
+
+    #[test]
+    fn execute_0x31() {
+        let (mut cpu, mut bus) = setup(&[0x34, 0x12]);
+
+        let cycles = cpu.execute(0x31, &mut bus);
+
+        assert_eq!(cycles, 12);
+        assert_eq!(cpu.registers.sp, 0x1234);
+    }
+
+    test_ld_r_u8!(execute_0x3E, 0x3E, a, 0x12);
+
+    test_ld_r_r!(execute_0x40, 0x40, b, b, 0x12);
+    test_ld_r_r!(execute_0x41, 0x41, b, c, 0x12);
+    test_ld_r_r!(execute_0x42, 0x42, b, d, 0x12);
+    test_ld_r_r!(execute_0x43, 0x43, b, e, 0x12);
+    test_ld_r_r!(execute_0x44, 0x44, b, h, 0x12);
+    test_ld_r_r!(execute_0x45, 0x45, b, l, 0x12);
+    test_ld_r_r!(execute_0x47, 0x47, b, a, 0x12);
+
+    test_ld_r_r!(execute_0x48, 0x48, c, b, 0x12);
+    test_ld_r_r!(execute_0x49, 0x49, c, c, 0x12);
+    test_ld_r_r!(execute_0x4A, 0x4A, c, d, 0x12);
+    test_ld_r_r!(execute_0x4B, 0x4B, c, e, 0x12);
+    test_ld_r_r!(execute_0x4C, 0x4C, c, h, 0x12);
+    test_ld_r_r!(execute_0x4D, 0x4D, c, l, 0x12);
+    test_ld_r_r!(execute_0x4F, 0x4F, c, a, 0x12);
+
+    test_ld_r_r!(execute_0x50, 0x50, d, b, 0x12);
+    test_ld_r_r!(execute_0x51, 0x51, d, c, 0x12);
+    test_ld_r_r!(execute_0x52, 0x52, d, d, 0x12);
+    test_ld_r_r!(execute_0x53, 0x53, d, e, 0x12);
+    test_ld_r_r!(execute_0x54, 0x54, d, h, 0x12);
+    test_ld_r_r!(execute_0x55, 0x55, d, l, 0x12);
+    test_ld_r_r!(execute_0x57, 0x57, d, a, 0x12);
+
+    test_ld_r_r!(execute_0x58, 0x58, e, b, 0x12);
+    test_ld_r_r!(execute_0x59, 0x59, e, c, 0x12);
+    test_ld_r_r!(execute_0x5A, 0x5A, e, d, 0x12);
+    test_ld_r_r!(execute_0x5B, 0x5B, e, e, 0x12);
+    test_ld_r_r!(execute_0x5C, 0x5C, e, h, 0x12);
+    test_ld_r_r!(execute_0x5D, 0x5D, e, l, 0x12);
+    test_ld_r_r!(execute_0x5F, 0x5F, e, a, 0x12);
+
+    test_ld_r_r!(execute_0x60, 0x60, h, b, 0x12);
+    test_ld_r_r!(execute_0x61, 0x61, h, c, 0x12);
+    test_ld_r_r!(execute_0x62, 0x62, h, d, 0x12);
+    test_ld_r_r!(execute_0x63, 0x63, h, e, 0x12);
+    test_ld_r_r!(execute_0x64, 0x64, h, h, 0x12);
+    test_ld_r_r!(execute_0x65, 0x65, h, l, 0x12);
+    test_ld_r_r!(execute_0x67, 0x67, h, a, 0x12);
+
+    test_ld_r_r!(execute_0x68, 0x68, l, b, 0x12);
+    test_ld_r_r!(execute_0x69, 0x69, l, c, 0x12);
+    test_ld_r_r!(execute_0x6A, 0x6A, l, d, 0x12);
+    test_ld_r_r!(execute_0x6B, 0x6B, l, e, 0x12);
+    test_ld_r_r!(execute_0x6C, 0x6C, l, h, 0x12);
+    test_ld_r_r!(execute_0x6D, 0x6D, l, l, 0x12);
+    test_ld_r_r!(execute_0x6F, 0x6F, l, a, 0x12);
+
+    test_ld_r_r!(execute_0x78, 0x78, a, b, 0x12);
+    test_ld_r_r!(execute_0x79, 0x79, a, c, 0x12);
+    test_ld_r_r!(execute_0x7A, 0x7A, a, d, 0x12);
+    test_ld_r_r!(execute_0x7B, 0x7B, a, e, 0x12);
+    test_ld_r_r!(execute_0x7C, 0x7C, a, h, 0x12);
+    test_ld_r_r!(execute_0x7D, 0x7D, a, l, 0x12);
+    test_ld_r_r!(execute_0x7F, 0x7F, a, a, 0x12);
+
+    #[test]
+    fn execute_0xF9() {
+        let (mut cpu, mut bus) = setup(&[]);
+        cpu.registers.set_hl(0x1234);
+
+        let cycles = cpu.execute(0xF9, &mut bus);
+
         assert_eq!(cycles, 8);
-        assert_eq!(cpu.registers.c, 0x12);
+        assert_eq!(cpu.registers.sp, 0x1234);
     }
 
-    #[test]
-    fn execute_0x11() {
-        // LD DE u16, set DE to u16, takes 12 cycles
-        let (mut cpu, mut bus) = setup(&[0x34, 0x12]);
-
-        let cycles = cpu.execute(0x11, &mut bus);
-
-        assert_eq!(cycles, 12);
-        assert_eq!(cpu.registers.get_de(), 0x1234);
-    }
-
-    #[test]
-    fn execute_0x21() {
-        // LD HL, u16, sets HL to u16, takes 12 cycles
-        let (mut cpu, mut bus) = setup(&[0x34, 0x12]);
-
-        let cycles = cpu.execute(0x21, &mut bus);
-
-        assert_eq!(cycles, 12);
-        assert_eq!(cpu.registers.get_hl(), 0x1234);
-    }
-
-    #[test]
-    fn execute_0x40() {
-        // LD B, B
-        let (mut cpu, mut bus) = setup(&[]);
-
-        let cycles = cpu.execute(0x40, &mut bus);
-        assert_eq!(cycles, 4);
-    }
-
-    #[test]
-    fn execute_0x41() {
-        // LD B, C
-        let (mut cpu, mut bus) = setup(&[]);
-        let cycles = cpu.execute(0x41, &mut bus);
-        assert_eq!(cycles, 4);
-        assert_eq!(cpu.registers.b, cpu.registers.c);
-    }
-
-    #[test]
-    fn execute_0x47() {
-        // LD B, A, sets B to value at A, takes 4 cycles
-        let (mut cpu, mut bus) = setup(&[]);
-        cpu.registers.a = 1;
-        cpu.registers.b = 5;
-
-        let cycles = cpu.execute(0x47, &mut bus);
-
-        assert_eq!(cycles, 4);
-        assert_eq!(cpu.registers.b, 1);
-        assert_eq!(cpu.registers.a, 1);
-    }
-
-    // todo: move this to the jump tests
     #[test]
     fn execute_0xC3() {
-        // JP u16, sets pc to u16, takes 16 cycles
         let (mut cpu, mut bus) = setup(&[0x34, 0x12]);
 
         let cycles = cpu.execute(0xC3, &mut bus);
--- emulator/src/cpu/op_rload.rs
+++ emulator/src/cpu/op_rload.rs
@@ -1,25 +1,45 @@
 use crate::{bus::Bus, cpu::Cpu};
 
+macro_rules! ld_r_u8 {
+    ($name:ident, $register:ident) => {
+        pub(super) fn $name(&mut self, bus: &mut Bus) -> u8 {
+            let value = self.fetch_u8(bus);
+            self.registers.$register = value;
+            8
+        }
+    };
+}
+
+macro_rules! ld_rr_u16 {
+    ($name:ident, $setter:ident) => {
+        pub(super) fn $name(&mut self, bus: &mut Bus) -> u8 {
+            let value = self.fetch_u16(bus);
+            self.registers.$setter(value);
+            12
+        }
+    };
+}
+
+macro_rules! ld_r_r {
+    ($name:ident, $dst:ident, $src:ident) => {
+        pub(super) fn $name(&mut self) -> u8 {
+            self.registers.$dst = self.registers.$src;
+            4
+        }
+    };
+}
+
 impl Cpu {
-    // 0x0E: LD C u8, set C to u8, takes 8 cycles
-    pub(super) fn ld_c_u8(&mut self, bus: &mut Bus) -> u8 {
-        let value = self.fetch_u8(bus);
-        self.registers.c = value;
-        8
-    }
-    // 0x11: LD DE u16, set DE to u16, takes 12 cycles
-    pub(super) fn ld_de_u16(&mut self, bus: &mut Bus) -> u8 {
+    ld_rr_u16!(ld_bc_u16, set_bc);
+    ld_rr_u16!(ld_de_u16, set_de);
+    ld_rr_u16!(ld_hl_u16, set_hl);
+
+    pub(super) fn ld_sp_u16(&mut self, bus: &mut Bus) -> u8 {
         let value = self.fetch_u16(bus);
-        self.registers.set_de(value);
+        self.registers.sp = value;
         12
     }
-    // 0x21: LD HL, u16, sets HL to u16, takes 12 cycles
-    pub(super) fn ld_hl_u16(&mut self, bus: &mut Bus) -> u8 {
-        let value = self.fetch_u16(bus);
-        self.registers.set_hl(value);
-        12
-    }
-    // 0x2A: LD A, (HL+), sets A to mem[HL++], takes 8 cycles
+
     pub(super) fn ld_a_hli(&mut self, bus: &mut Bus) -> u8 {
         let value = bus.read_u8(self.registers.get_hl());
         self.registers.a = value;
@@ -27,20 +47,72 @@ impl Cpu {
         8
     }
 
-    // 0x40: LD B, B
-    pub(super) fn ld_b_b(&mut self) -> u8 {
-        4
-    }
+    ld_r_u8!(ld_b_u8, b);
+    ld_r_u8!(ld_c_u8, c);
+    ld_r_u8!(ld_d_u8, d);
+    ld_r_u8!(ld_e_u8, e);
+    ld_r_u8!(ld_h_u8, h);
+    ld_r_u8!(ld_l_u8, l);
+    ld_r_u8!(ld_a_u8, a);
 
-    // 0x41: LD B, C
-    pub(super) fn ld_b_c(&mut self) -> u8 {
-        self.registers.b = self.registers.c;
-        4
-    }
+    ld_r_r!(ld_b_b, b, b);
+    ld_r_r!(ld_b_c, b, c);
+    ld_r_r!(ld_b_d, b, d);
+    ld_r_r!(ld_b_e, b, e);
+    ld_r_r!(ld_b_h, b, h);
+    ld_r_r!(ld_b_l, b, l);
+    ld_r_r!(ld_b_a, b, a);
 
-    // 0x47:
-    pub(super) fn ld_b_a(&mut self) -> u8 {
-        self.registers.b = self.registers.a;
-        4
+    ld_r_r!(ld_c_b, c, b);
+    ld_r_r!(ld_c_c, c, c);
+    ld_r_r!(ld_c_d, c, d);
+    ld_r_r!(ld_c_e, c, e);
+    ld_r_r!(ld_c_h, c, h);
+    ld_r_r!(ld_c_l, c, l);
+    ld_r_r!(ld_c_a, c, a);
+
+    ld_r_r!(ld_d_b, d, b);
+    ld_r_r!(ld_d_c, d, c);
+    ld_r_r!(ld_d_d, d, d);
+    ld_r_r!(ld_d_e, d, e);
+    ld_r_r!(ld_d_h, d, h);
+    ld_r_r!(ld_d_l, d, l);
+    ld_r_r!(ld_d_a, d, a);
+
+    ld_r_r!(ld_e_b, e, b);
+    ld_r_r!(ld_e_c, e, c);
+    ld_r_r!(ld_e_d, e, d);
+    ld_r_r!(ld_e_e, e, e);
+    ld_r_r!(ld_e_h, e, h);
+    ld_r_r!(ld_e_l, e, l);
+    ld_r_r!(ld_e_a, e, a);
+
+    ld_r_r!(ld_h_b, h, b);
+    ld_r_r!(ld_h_c, h, c);
+    ld_r_r!(ld_h_d, h, d);
+    ld_r_r!(ld_h_e, h, e);
+    ld_r_r!(ld_h_h, h, h);
+    ld_r_r!(ld_h_l, h, l);
+    ld_r_r!(ld_h_a, h, a);
+
+    ld_r_r!(ld_l_b, l, b);
+    ld_r_r!(ld_l_c, l, c);
+    ld_r_r!(ld_l_d, l, d);
+    ld_r_r!(ld_l_e, l, e);
+    ld_r_r!(ld_l_h, l, h);
+    ld_r_r!(ld_l_l, l, l);
+    ld_r_r!(ld_l_a, l, a);
+
+    ld_r_r!(ld_a_b, a, b);
+    ld_r_r!(ld_a_c, a, c);
+    ld_r_r!(ld_a_d, a, d);
+    ld_r_r!(ld_a_e, a, e);
+    ld_r_r!(ld_a_h, a, h);
+    ld_r_r!(ld_a_l, a, l);
+    ld_r_r!(ld_a_a, a, a);
+
+    pub(super) fn ld_sp_hl(&mut self) -> u8 {
+        self.registers.sp = self.registers.get_hl();
+        8
     }
 }
