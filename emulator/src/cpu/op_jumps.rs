02bc7af1fc05cd14aedfe966704049abdff41b3d
Young Seo Kim <youngseokim7@gmail.com>
2026-04-24 17:54:05 +0900

    implement relative jump (JR) instructions

--- emulator/src/cpu/op_jumps.rs
+++ emulator/src/cpu/op_jumps.rs
@@ -11,6 +11,38 @@ impl Cpu {
         }
     }
 
+    fn conditional_rel_jump(&mut self, bus: &mut Bus, condition: bool) -> u8 {
+        let value = self.fetch_u8(bus) as i8;
+        if condition {
+            self.registers.pc = self.registers.pc.wrapping_add_signed(value as i16);
+            3
+        } else {
+            2
+        }
+    }
+
+    // ========== RELATIVE JUMPS =============
+    pub(super) fn jr_i8(&mut self, bus: &mut Bus) -> u8 {
+        self.conditional_rel_jump(bus, true)
+    }
+
+    pub(super) fn jr_nz_i8(&mut self, bus: &mut Bus) -> u8 {
+        self.conditional_rel_jump(bus, !self.registers.get_z())
+    }
+
+    pub(super) fn jr_z_i8(&mut self, bus: &mut Bus) -> u8 {
+        self.conditional_rel_jump(bus, self.registers.get_z())
+    }
+
+    pub(super) fn jr_nc_i8(&mut self, bus: &mut Bus) -> u8 {
+        self.conditional_rel_jump(bus, !self.registers.get_c())
+    }
+
+    pub(super) fn jr_c_i8(&mut self, bus: &mut Bus) -> u8 {
+        self.conditional_rel_jump(bus, self.registers.get_c())
+    }
+
+    // =========== ABSOLUTE JUMPS ==================
     pub(super) fn jp_nz_u16(&mut self, bus: &mut Bus) -> u8 {
         self.conditional_jump(bus, !self.registers.get_z())
     }
