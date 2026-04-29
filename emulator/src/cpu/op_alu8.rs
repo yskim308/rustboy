835fb6ca1149faab4d8e924c20013a7d365c8f10
Young Seo Kim <youngseokim7@gmail.com>
2026-04-29 20:50:58 +0900

    refactor add_a_u8 helper and implement ADC instructions

--- emulator/src/cpu/op_alu8.rs
+++ emulator/src/cpu/op_alu8.rs
@@ -3,24 +3,39 @@ use crate::{bus::Bus, cpu::Cpu};
 macro_rules! add_a_r {
     ($func_name: ident, $source_r: ident) => {
         pub(super) fn $func_name(&mut self) -> u8 {
-            self.add_a_u8(self.registers.$source_r);
+            self.add_a_u8(self.registers.$source_r, false);
+            4
+        }
+    };
+}
+
+macro_rules! adc_a_r {
+    ($func_name: ident, $source_r: ident) => {
+        pub(super) fn $func_name(&mut self) -> u8 {
+            self.add_a_u8(self.registers.$source_r, self.registers.get_c());
             4
         }
     };
 }
 
 impl Cpu {
-    // ============= ADD instructions ==================
-    fn add_a_u8(&mut self, val: u8) {
-        let source_val = val;
-        let (result, carry) = self.registers.a.overflowing_add(source_val);
-        let half_carry = (self.registers.a & 0x0F) + (source_val & 0x0F) > 0x0F;
+    // ============= ADD/ADC instructions ==================
+    fn add_a_u8(&mut self, val: u8, is_carry: bool) {
+        let a = self.registers.a as u16;
+        let source_val = val as u16;
+        let carry_in = is_carry as u16;
+
+        let result = a + source_val + carry_in;
+        let half_carry = (a & 0x0F) + (source_val & 0x0F) + carry_in > 0x0F;
+        let carry_out = result > 0xFF;
+
+        let result = result as u8;
         self.registers.a = result;
 
         self.registers.set_z(result == 0);
         self.registers.set_n(false);
         self.registers.set_h(half_carry);
-        self.registers.set_c(carry);
+        self.registers.set_c(carry_out);
     }
 
     add_a_r!(add_a_b, b);
@@ -32,7 +47,22 @@ impl Cpu {
     add_a_r!(add_a_a, a);
 
     pub(super) fn add_a_at_hl(&mut self, bus: &mut Bus) -> u8 {
-        self.add_a_u8(bus.read_u8(self.registers.get_hl()));
+        let val_at_hl = bus.read_u8(self.registers.get_hl());
+        self.add_a_u8(val_at_hl, false);
+        8
+    }
+
+    adc_a_r!(adc_a_b, b);
+    adc_a_r!(adc_a_c, c);
+    adc_a_r!(adc_a_d, d);
+    adc_a_r!(adc_a_e, e);
+    adc_a_r!(adc_a_h, h);
+    adc_a_r!(adc_a_l, l);
+    adc_a_r!(adc_a_a, a);
+
+    pub(super) fn adc_a_at_hl(&mut self, bus: &mut Bus) -> u8 {
+        let val_at_hl = bus.read_u8(self.registers.get_hl());
+        self.add_a_u8(val_at_hl, self.registers.get_c());
         8
     }
 }
