e2821f70753185ac6b1196c30699e4b1e5eddcbf
Young Seo Kim <youngseokim7@gmail.com>
2026-05-15 19:24:06 +0900

    implement vlbank timing, STAT, LYC, and DMA transfer and set IO register to post-boot defaults

--- emulator/src/bus.rs
+++ emulator/src/bus.rs
@@ -39,7 +39,12 @@ impl Bus {
             obp1: self.read_u8(0xFF49),
             bgp: self.read_u8(0xFF47),
         };
-        self.ppu.step(cycles, ppu_registers);
+        let entered_vblank = self.ppu.step(cycles, ppu_registers);
+        self.io_bucket[(0xFF44 - 0xFF00) as usize] = self.ppu.ly();
+
+        if entered_vblank {
+            self.io_bucket[(0xFF0F - 0xFF00) as usize] |= 0x01;
+        }
     }
 
     pub fn get_ppu_frame_buffer(&self) -> &[u8] {
@@ -47,12 +52,15 @@ impl Bus {
     }
 
     pub fn new(cartridge: Cartridge) -> Self {
+        let mut io_bucket = [0; 128];
+        set_post_boot_io_defaults(&mut io_bucket);
+
         Self {
             cartridge,
             eram: [0; 8192],
             wram: Wram::new(),
             serial: Serial::new(),
-            io_bucket: [0; 128],
+            io_bucket,
             hram: [0; 127],
             ie_register: 0,
             ppu: Ppu::default(),
@@ -63,7 +71,7 @@ impl Bus {
         match address {
             0x0000..=0x7FFF => self.cartridge.read_u8(address),
             0x8000..=0x9FFF => self.ppu.read_u8(address),
-            0xA000..=0xBBFF => self.eram[(address - 0xA000) as usize],
+            0xA000..=0xBFFF => self.eram[(address - 0xA000) as usize],
             0xC000..=0xDFFF => self.wram.read_u8(address - 0xC000),
             0xE000..=0xFDFF => self.wram.read_u8(address - 0xE000), // echo ram
             0xFE00..=0xFE9F => self.ppu.read_u8(address),
@@ -76,9 +84,9 @@ impl Bus {
 
     pub fn write_u8(&mut self, address: u16, data: u8) {
         match address {
-            0x0000..=0x7FF => println!("accessed ROM as write for MBC"),
+            0x0000..=0x7FFF => println!("accessed ROM as write for MBC"),
             0x8000..=0x9FFF => self.ppu.write_u8(address, data),
-            0xA000..=0xBBFF => self.eram[(address - 0xA000) as usize] = data,
+            0xA000..=0xBFFF => self.eram[(address - 0xA000) as usize] = data,
             0xC000..=0xDFFF => self.wram.write_u8(address - 0xC000, data),
             0xE000..=0xFDFF => self.wram.write_u8(address - 0xE000, data), // echo ram
             0xFE00..=0xFE9F => self.ppu.write_u8(address, data),
@@ -92,6 +100,11 @@ impl Bus {
     fn read_io(&self, address: u16) -> u8 {
         match address {
             0xFF01..=0xFF02 => self.serial.read(address),
+            0xFF41 => self.ppu.stat(
+                self.io_bucket[(0xFF41 - 0xFF00) as usize],
+                self.io_bucket[(0xFF45 - 0xFF00) as usize],
+            ),
+            0xFF44 => self.ppu.ly(),
             _ => self.io_bucket[(address - 0xFF00) as usize],
         }
     }
@@ -99,7 +112,66 @@ impl Bus {
     fn write_io(&mut self, address: u16, data: u8) {
         match address {
             0xFF01..=0xFF02 => self.serial.write(address, data),
+            0xFF46 => self.oam_dma_transfer(data),
+            0xFF44 => {
+                self.ppu.reset_ly();
+                self.io_bucket[(address - 0xFF00) as usize] = 0;
+            }
             _ => self.io_bucket[(address - 0xFF00) as usize] = data,
         }
+    }
+
+    fn oam_dma_transfer(&mut self, data: u8) {
+        let base_address = (data as u16) << 8;
+
+        for offset in 0..160u16 {
+            let value = self.read_u8(base_address + offset);
+            self.ppu.write_u8(0xFE00 + offset, value);
+        }
+
+        self.io_bucket[(0xFF46 - 0xFF00) as usize] = data;
+    }
+}
+
+fn set_post_boot_io_defaults(io_bucket: &mut [u8; 128]) {
+    let defaults = [
+        (0xFF00, 0xCF),
+        (0xFF05, 0x00),
+        (0xFF06, 0x00),
+        (0xFF07, 0x00),
+        (0xFF10, 0x80),
+        (0xFF11, 0xBF),
+        (0xFF12, 0xF3),
+        (0xFF14, 0xBF),
+        (0xFF16, 0x3F),
+        (0xFF17, 0x00),
+        (0xFF19, 0xBF),
+        (0xFF1A, 0x7F),
+        (0xFF1B, 0xFF),
+        (0xFF1C, 0x9F),
+        (0xFF1E, 0xBF),
+        (0xFF20, 0xFF),
+        (0xFF21, 0x00),
+        (0xFF22, 0x00),
+        (0xFF23, 0xBF),
+        (0xFF24, 0x77),
+        (0xFF25, 0xF3),
+        (0xFF26, 0xF1),
+        (0xFF40, 0x91),
+        (0xFF41, 0x85),
+        (0xFF42, 0x00),
+        (0xFF43, 0x00),
+        (0xFF44, 0x00),
+        (0xFF45, 0x00),
+        (0xFF47, 0xFC),
+        (0xFF48, 0xFF),
+        (0xFF49, 0xFF),
+        (0xFF4A, 0x00),
+        (0xFF4B, 0x00),
+        (0xFF0F, 0xE1),
+    ];
+
+    for (address, value) in defaults {
+        io_bucket[(address - 0xFF00) as usize] = value;
     }
 }
